efb8fb07c7fceb9166f7a8b1224c4e04bf639b53
Leandro Bertoluzzi <leajb10@gmail.com>
2024-06-12 17:16:03 -0300

    feat: Added endpoint to execute JOG command

--- routes/cncRoutes.py
+++ routes/cncRoutes.py
@@ -1,18 +1,29 @@
 from config import SERIAL_PORT, SERIAL_BAUDRATE
 from core.cncworker.workerStatusManager import WorkerStoreAdapter
 import core.cncworker.utils as worker
+import core.grbl.grblUtils as grblUtils
 from core.utils.serial import SerialService
 from core.worker import cncServer, COMMANDS_CHANNEL
 from fastapi import APIRouter, HTTPException
 from middleware.authMiddleware import GetAdminDep
 from middleware.pubSubMiddleware import GetPubSub
 from pydantic import BaseModel
+from typing import Literal
 
 cncRoutes = APIRouter(prefix="/cnc", tags=["CNC"])
 
 
 class CncCommandModel(BaseModel):
     command: str
+
+
+class CncJogCommandModel(BaseModel):
+    x: float
+    y: float
+    z: float
+    feedrate: float
+    units: Literal["milimeters", "inches"]
+    mode: Literal["distance_absolute", "distance_incremental"]
 
 
 @cncRoutes.get('/ports')
@@ -77,3 +88,36 @@ async def send_code_to_execute(
     await redis.publish(COMMANDS_CHANNEL, code)
 
     return {'success': 'El comando fue enviado para su ejecución'}
+
+
+@cncRoutes.post('/jog')
+async def send_jog_command(
+    admin: GetAdminDep,
+    redis: GetPubSub,
+    request: CncJogCommandModel,
+    machine: bool = False
+):
+    if not worker.is_worker_on():
+        raise HTTPException(400, detail='Worker desconectado')
+
+    if not worker.is_worker_running():
+        raise HTTPException(400, detail='Debe iniciar la conexión con el servidor CNC')
+
+    # Generate code from request body
+    code = grblUtils.build_jog_command(
+        request.x,
+        request.y,
+        request.z,
+        request.feedrate,
+        units=request.units,
+        distance_mode=request.mode,
+        machine_coordinates=machine
+    )
+
+    # Request command execution
+    await redis.publish(COMMANDS_CHANNEL, code)
+
+    return {
+        'success': 'El comando fue enviado para su ejecución',
+        'command': code
+    }
